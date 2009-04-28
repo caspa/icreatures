0e80eac456aaaaac4094a1cc5e91c9369db533b6
Andre Bruehl <andrebruehl@googlemail.com>
2009-04-28 13:40:57 +0200

    mooshine config

--- app/manifests/application_manifest.rb
+++ app/manifests/application_manifest.rb
@@ -27,7 +27,7 @@ class ApplicationManifest < Moonshine::Manifest::Rails
     # If you've already told Moonshine about a package required by a gem with
     # :apt_gems in <tt>moonshine.yml</tt> you do not need to include it here.
     # package 'some_native_package', :ensure => :installed
-    
+     package 'imagemagick', :ensure => :installed
     # some_rake_task = "/usr/bin/rake -f #{configuration[:deploy_to]}/current/Rakefile custom:task RAILS_ENV=#{ENV['RAILS_ENV']}"
     # cron 'custom:task', :command => some_rake_task, :user => configuration[:user], :minute => 0, :hour => 0
     
